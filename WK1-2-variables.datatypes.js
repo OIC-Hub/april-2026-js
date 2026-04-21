e3572a58a9f3b0124fc517bb9ef0a3017175983e
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-04-21 13:08:50 +0100

    added class files on variables and datatypes

---
+++ WK1-2-variables.datatypes.js
@@ -0,0 +1,77 @@
+var firstname = "John";
+
+console.log(firstname);
+
+firstname = "Ade";
+
+console.log(firstname);
+
+let age = 11;
+console.log(age);
+
+age = 14;
+
+console.log(age);
+
+let lastName = "Doe";
+console.log(lastName);
+
+const pi = 3.142;
+// pi = 20;
+// const pi = 21;
+console.log(pi);
+
+const gravity = 10;
+console.log(gravity);
+
+{
+    var firstname = "Tolu";
+    console.log(firstname);
+
+    let age = 18;
+    console.log(age);
+
+    lastName = "Lawal";
+    console.log(lastName);
+    
+    const pi = 24;
+    console.log(pi);
+    
+    // gravity = 21;
+}
+
+console.log(firstname);
+console.log(age);
+console.log(lastName);
+console.log(pi);
+console.log(gravity);
+
+console.log(typeof firstname);
+console.log(typeof age);
+
+let sentence1 = "Ade's son is going to school";
+console.log(sentence1);
+
+let sentence2 = 'Messi is the "greatest" of all time';
+console.log(sentence2);
+
+let sentence3 = `Ade's son is very "stubborn" and jovial`;
+console.log(sentence3);
+
+let sentence4 = "Oga Niyi is very calm.";
+let sentence5 = "He is also very handsome.";
+
+let sentence6 = sentence4 + " " + sentence5;
+console.log(sentence6);
+
+let sentence7 = `Ade is ${age} years old`;
+console.log(sentence7);
+
+let sentence8 = `${sentence4} ${sentence5}`;
+console.log(sentence8);
+
+
+
+
+
+
