a31c4d6c37040ec23f3263e0dec94c42a8a01faa
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-04-22 12:08:12 +0100

    added class files on operators

---
+++ WK1-3-operators.js
@@ -0,0 +1,106 @@
+let a = 10;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(17 % 9);
+console.log(14 % 3);
+console.log(a ** b);
+console.log(2 ** 3);
+
+let c = 9;
+console.log(c);
+console.log(c++);
+console.log(c);
+
+let d = 5;
+console.log(d);
+d++;
+console.log(d);
+
+let e = 4;
+console.log(e);
+console.log(++e);
+console.log(e);
+
+let f = 5;
+console.log(f--);
+console.log(f);
+
+let g = 10;
+console.log(--g);
+console.log(g);
+
+let sum = 2 + 4 + "Ade";
+console.log(sum);
+console.log(2 + 4 + "7");
+console.log("7" + 2 + 4);
+console.log(11 + 2 + "5" + 2 + 1);
+console.log("3" * 2);
+
+console.log(4 + (3 * 2) - 9 * 5);
+
+
+let h = 9;
+h += 3;
+console.log(h);
+
+let i = 12;
+i -= 15;
+console.log(i);
+
+let j = 3;
+j *= 4;
+console.log(j);
+
+let k = 6;
+k /= 4;
+console.log(k);
+
+let l = 3;
+l %= 4;
+console.log(l);
+
+console.log(5 > 3);
+console.log(-5 > -3);
+console.log(-1 < -2);
+console.log(2 < 2);
+
+console.log(-3 >= -1);
+console.log(3 >= 3);
+console.log(-5 <= -2);
+console.log(3 == 3);
+console.log(4 == "4");
+console.log(4 == "four");
+
+console.log(4 === 4);
+console.log(4 === "4");
+
+console.log(4 != 4);
+console.log(4 != "4");
+console.log(4 != 6);
+
+console.log(4 !== "4");
+console.log(4 !== 4);
+console.log(4 !== 9);
+
+console.log((4 > 3) || (-1 > 0));
+
+console.log((4 < -5) && (2 > 1) && (4 != -1));
+
+console.log(!(4 > 5));
+
+let num1 = 5 > 3 ? 11 : 19;
+console.log(num1);
+
+console.log(((7 >= 7) && (3 < 5) || (10 === 2)) && ((8 != 4) ? (6 > 2) : (9 < 1)));
+console.log(((5 > 3) && !(2 === 2) || (4 <= 4)) && ((3 != 1) ? ((2 < 5) && (7 > 6)) : (8 === 9)));
+
+
+
+
+
+
